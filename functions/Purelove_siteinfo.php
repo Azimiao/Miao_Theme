0e02056c8abee33baaa6bce7572fec03f3b12d3f
Yetu <admin@azimiao.com>
2018-02-13 23:49:53 +0800

    删除讨厌的^M

--- functions/Purelove_siteinfo.php
+++ functions/Purelove_siteinfo.php
@@ -1,1 +1,109 @@
-<?phpclass Purelove_siteinfo extends WP_Widget {    function Purelove_siteinfo() {        $widget_ops = array('description' => '网站统计功能');        $this->WP_Widget('Purelove_siteinfo', 'Purelove_网站统计', $widget_ops);    }    function widget($args, $instance) {        extract($args);        echo $before_widget;        $start=Purelove_get_firstpostdate();?>    <h3>网站统计</h3>        <ul class="blogroll clearfix">            <li>文章总数：<?php $count_posts = wp_count_posts();echo $published_posts = $count_posts->publish;?>篇</li>            <li>评论总数：<?php $count_comments = get_comment_count();echo $count_comments['approved'];?>条</li>            <li>页面总数：<?php $count_pages = wp_count_posts('page'); echo $page_posts = $count_pages->publish; ?> 个</li>            <li>分类总数：<?php echo $count_categories = wp_count_terms('category'); ?>个</li>            <li>标签总数：<?php echo $count_tags = wp_count_terms('post_tag'); ?>个</li>            <li>网站运行：<?php echo floor((time()-strtotime($start))/86400); ?>天</li>        </ul> <?php          echo $after_widget;   }    function form($instance) {        global $wpdb;?>    <p>该工具没有选项!</p><?php    }}add_action('widgets_init', 'Purelove_siteinfo_init');function Purelove_siteinfo_init() {    register_widget('Purelove_siteinfo');}?>
+<?php
+
+
+
+
+
+class Purelove_siteinfo extends WP_Widget {
+
+
+    function Purelove_siteinfo() {
+
+
+        $widget_ops = array('description' => '网站统计功能');
+
+
+        $this->WP_Widget('Purelove_siteinfo', 'Purelove_网站统计', $widget_ops);
+
+
+    }
+
+
+    function widget($args, $instance) {
+
+
+        extract($args);
+
+
+        echo $before_widget;
+
+
+        $start=Purelove_get_firstpostdate();
+
+
+?>
+
+
+    <h3>网站统计</h3>
+
+
+        <ul class="blogroll clearfix">
+
+
+            <li>文章总数：<?php $count_posts = wp_count_posts();echo $published_posts = $count_posts->publish;?>篇</li>
+
+
+            <li>评论总数：<?php $count_comments = get_comment_count();echo $count_comments['approved'];?>条</li>
+
+
+            <li>页面总数：<?php $count_pages = wp_count_posts('page'); echo $page_posts = $count_pages->publish; ?> 个</li>
+
+
+            <li>分类总数：<?php echo $count_categories = wp_count_terms('category'); ?>个</li>
+
+
+            <li>标签总数：<?php echo $count_tags = wp_count_terms('post_tag'); ?>个</li>
+
+
+            <li>网站运行：<?php echo floor((time()-strtotime($start))/86400); ?>天</li>
+
+
+        </ul> 
+
+
+<?php    
+
+
+      echo $after_widget;
+
+
+   }
+
+
+    function form($instance) {
+
+
+        global $wpdb;
+
+
+?>
+
+
+    <p>该工具没有选项!</p>
+
+
+<?php
+
+
+    }
+
+
+}
+
+
+add_action('widgets_init', 'Purelove_siteinfo_init');
+
+
+function Purelove_siteinfo_init() {
+
+
+    register_widget('Purelove_siteinfo');
+
+
+}
+
+
+
+
+
+?>
--- functions/Purelove_wanted.php
+++ functions/Purelove_wanted.php
@@ -1,1 +1,193 @@
-<?php  //widget Purelove_wantedadd_action('widgets_init', create_function('', 'return register_widget("Purelove_wanted");'));class Purelove_wanted extends WP_Widget {	function Purelove_wanted() {		global $prename;		$this->WP_Widget('Purelove_wanted', $prename.'Purelove_置顶推荐 ', array( 'description' => '显示置顶文章' ));	}	function widget($args, $instance) {		extract($args, EXTR_SKIP);		echo $before_widget;		$title = apply_filters('widget_name', $instance['title']);		$limit = $instance['limit'];		echo $before_title.$title.$after_title; 		echo '<ul>';		echo Purelove_wantedlists( $limit );;		echo '</ul>';		echo $after_widget;	}	function update($new_instance, $old_instance) {		$instance = $old_instance;		$instance['title'] = strip_tags($new_instance['title']);		$instance['limit'] = strip_tags($new_instance['limit']);		return $instance;	}	function form($instance) {		$instance = wp_parse_args( (array) $instance, array( 			'title' => '置顶推荐',			'limit' => '10' 			) 		);		$title = strip_tags($instance['title']);		$limit = strip_tags($instance['limit']);?>		<p>			<label>				标题：				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" />			</label>		</p>		<p>			<label>				显示数目：				<input class="widefat" id="<?php echo $this->get_field_id('limit'); ?>" name="<?php echo $this->get_field_name('limit'); ?>" type="number" value="<?php echo $instance['limit']; ?>" />			</label>		</p><?php	}}function Purelove_wantedlists($limit){	$sticky = get_option('sticky_posts');rsort( $sticky );$sticky = array_slice( $sticky, 0, $limit );	query_posts( array( 'post__in' => $sticky, 'caller_get_posts' => 1 ) );	while (have_posts()) : the_post();	echo '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';	endwhile; wp_reset_query();}?>
+<?php  
+
+
+
+
+
+//widget Purelove_wanted
+
+
+
+
+
+add_action('widgets_init', create_function('', 'return register_widget("Purelove_wanted");'));
+
+
+
+
+
+class Purelove_wanted extends WP_Widget {
+
+
+	function Purelove_wanted() {
+
+
+		global $prename;
+
+
+		$this->WP_Widget('Purelove_wanted', $prename.'Purelove_置顶推荐 ', array( 'description' => '显示置顶文章' ));
+
+
+	}
+
+
+	function widget($args, $instance) {
+
+
+		extract($args, EXTR_SKIP);
+
+
+		echo $before_widget;
+
+
+		$title = apply_filters('widget_name', $instance['title']);
+
+
+		$limit = $instance['limit'];
+
+
+
+
+
+		echo $before_title.$title.$after_title; 
+
+
+		echo '<ul>';
+
+
+		echo Purelove_wantedlists( $limit );;
+
+
+		echo '</ul>';
+
+
+		echo $after_widget;
+
+
+	}
+
+
+	function update($new_instance, $old_instance) {
+
+
+		$instance = $old_instance;
+
+
+		$instance['title'] = strip_tags($new_instance['title']);
+
+
+		$instance['limit'] = strip_tags($new_instance['limit']);
+
+
+		return $instance;
+
+
+	}
+
+
+	function form($instance) {
+
+
+		$instance = wp_parse_args( (array) $instance, array( 
+
+
+			'title' => '置顶推荐',
+
+
+			'limit' => '10' 
+
+
+			) 
+
+
+		);
+
+
+		$title = strip_tags($instance['title']);
+
+
+		$limit = strip_tags($instance['limit']);
+
+
+?>
+
+
+		<p>
+
+
+			<label>
+
+
+				标题：
+
+
+				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" />
+
+
+			</label>
+
+
+		</p>
+
+
+		<p>
+
+
+			<label>
+
+
+				显示数目：
+
+
+				<input class="widefat" id="<?php echo $this->get_field_id('limit'); ?>" name="<?php echo $this->get_field_name('limit'); ?>" type="number" value="<?php echo $instance['limit']; ?>" />
+
+
+			</label>
+
+
+		</p>
+
+
+
+
+
+<?php
+
+
+	}
+
+
+}
+
+
+
+
+
+function Purelove_wantedlists($limit){
+
+
+	$sticky = get_option('sticky_posts');rsort( $sticky );$sticky = array_slice( $sticky, 0, $limit );
+
+
+	query_posts( array( 'post__in' => $sticky, 'caller_get_posts' => 1 ) );
+
+
+	while (have_posts()) : the_post();
+
+
+
+
+
+	echo '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
+
+
+	endwhile; wp_reset_query();
+
+
+}
+
+
+
+
+
+?>
