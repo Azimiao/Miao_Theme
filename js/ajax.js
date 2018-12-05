2d66730868598d6eec2fcf20f963fb9be6fde78e
Yetu <admin@azimiao.com>
2018-12-06 01:33:32 +0800

    新增：兼容回复按钮

--- js/ajax.js
+++ js/ajax.js
@@ -2,7 +2,7 @@
 var ajaxhome = ThemeConfig.ajaxHome;
 var ajaxcontent = new Array("navigation","content");
 var ajaxsearch_class = 'searchform';
-var ajaxignore_string = new String('#comments,#, /wp-, .pdf, .zip, .rar, /goto');
+var ajaxignore_string = new String('#comments,#, /wp-, .pdf, .zip, .rar, /goto, respond, reply');
 var ajaxignore = ajaxignore_string.split(', ');
 var ajaxloading_code = '<div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div><style>.spinner{position:absolute;z-index:999;left:48%;top:30%;width:50px;height:60px;text-align:center;font-size:10px;}.spinner>div{background-color:#ff8c83;height:100%;width:6px;display:inline-block;-webkit-animation:stretchdelay 1.2s infinite ease-in-out;animation:stretchdelay 1.2s infinite ease-in-out}.spinner .rect2{-webkit-animation-delay:-1.1s;animation-delay:-1.1s}.spinner .rect3{-webkit-animation-delay:-1.0s;animation-delay:-1.0s}.spinner .rect4{-webkit-animation-delay:-0.9s;animation-delay:-0.9s}.spinner .rect5{-webkit-animation-delay:-0.8s;animation-delay:-0.8s}@-webkit-keyframes stretchdelay{0%,40%,100%{-webkit-transform:scaleY(0.4)}20%{-webkit-transform:scaleY(1.0)}}@keyframes stretchdelay{0%,40%,100%{transform:scaleY(0.4);-webkit-transform:scaleY(0.4)}20%{transform:scaleY(1.0);-webkit-transform:scaleY(1.0)}}</style>';
 var ajaxloading_error_code = 'error';
